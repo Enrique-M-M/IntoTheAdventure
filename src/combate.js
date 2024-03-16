bef582ff2c52361ba3d0c7ec8dbba163bd70a964
EnriMo <emorenomozos13@gmail.com>
2024-03-16 20:58:28 +0100

    Select Tile version 2

--- src/combate.js
+++ src/combate.js
@@ -13,6 +13,9 @@ import tileSelected from './tileSelected.js';
  * @extends Phaser.Scene
  */
 export default class Combate extends Phaser.Scene {
+
+    neigbours = [[1,1],[0,1],[-1,1],[1,0],[0,0],[-1,0],[1,-1],[0,-1],[-1,-1]]
+
     constructor() {
         super({ key: 'Combate' });
     }
@@ -37,33 +40,63 @@ export default class Combate extends Phaser.Scene {
           //nombre de la paleta de tiles usado para pintar en tiled 
           //Mantener nombres constantes al crear mapa -> Tiles_Map
           const tileset1 = this.map.addTilesetImage('Tiles_Map', 'Tiles_Map');
-          this.backgroundLayer = this.map.createLayer('Capa_1', [tileset1]);
-          this.SelectedCapa = this.map.createLayer('SelectedCapa', [tileset1]);
-        
-        
+          this.capaSuelo = this.map.createLayer('Suelo', [tileset1]);
+          
+          this.capaSelect = this.map.createFromObjects('SelectLayer', {name: 'SelectIndicator',
+                                                        classType: Phaser.GameObjects.Sprite})
+          this.capaJuego = this.map.createLayer('CapaJuego', [tileset1]);
+          this.capaSelect.map(si => {
+            si.setTexture('mapIndicators', 4);
+            si.setScale(1,1);
+        });
+          this.capaSelect.map(si => si.setVisible(false));
+
           this.cameras.main.setZoom(4);
           this.cameras.main.scrollX = -this.map.widthInPixels / 2 - 425;
           this.cameras.main.scrollY = -this.map.heightInPixels / 2 - 260;
           console.log("pos cameras " +this.cameras.main.scrollX +" " +this.cameras.main.scrollY );
 
-          var sprite = this.add.sprite(0, 0, 'mapIndicators', 0);
-          sprite.setVisible(false);
-
+          
           this.input.on(Phaser.Input.Events.POINTER_UP, (pointer) => {
             const { worldX, worldY } = pointer;
     
-            const targetVec = this.SelectedCapa.worldToTileXY(worldX, worldY);
-
-            console.log("Click en casilla " + Math.trunc(targetVec.x) +" " +Math.trunc(targetVec.y ) );
-            console.log("Click en casilla " +worldX +" " +worldY );
-
-            sprite.setPosition(this.SelectedCapa.getTileAtWorldXY(worldX,worldY,true,this.cameras.main).getCenterX(),this.SelectedCapa.getTileAtWorldXY(worldX,worldY,true,this.cameras.main).getCenterY()+9);
-            sprite.setVisible(true);
+            const targetVec = this.capaJuego.worldToTileXY(worldX, worldY);
+            targetVec.x = Math.trunc(targetVec.x);
+            targetVec.y = Math.trunc(targetVec.y);
+            if(this.capaJuego.getTileAt(targetVec.x,targetVec.y,true).index != -1){
+                this.capaSelect.map(si =>{
+                     si.setTexture('mapIndicators', 2)
+                     si.y = this.capaJuego.getTileAt(targetVec.x,targetVec.y,true).getCenterY()+4;});
+            }
+            else{
+                this.capaSelect.map(si => {
+                    si.setTexture('mapIndicators', 4)
+                    si.y = this.capaJuego.getTileAt(targetVec.x,targetVec.y,true).getBottom()+4;
+                });
+                
+            }    
+            this.capaSelect.map(si => {
+                si.x = this.capaJuego.getTileAt(targetVec.x,targetVec.y,true).getCenterX();
+            });
+            this.capaSelect.map(si => si.setVisible(true));
+            this.capaJuego.forEachTile(tl => tl.setAlpha(1))
+            console.log("clicked on " + targetVec.x +" " + targetVec.y)
+            for(let cords of this.neigbours){
+                if( targetVec.x + cords[0] >= 0 && targetVec.x + cords[0] < this.map.height 
+                    && targetVec.y + cords[1] >= 0 && targetVec.y + cords[1] < this.map.height 
+                    && (this.capaJuego.getTileAt(targetVec.x + cords[0],targetVec.y +cords[1],true).index !== -1))
+                    {
+                        this.capaJuego.getTileAt(targetVec.x+ cords[0],targetVec.y +cords[1],true).setAlpha(0.6);
+                    }
+            }
             // use startVec and targetVec
         })
         this.events.on(Phaser.Scenes.Events.SHUTDOWN, () => {
             this.input.off(Phaser.Input.Events.POINTER_UP)
         })
 
-}
+    }
+    
+   
+        
 }
