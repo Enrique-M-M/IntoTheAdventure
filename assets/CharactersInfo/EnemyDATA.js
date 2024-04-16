6c5ccb3e7edf11cf733c5511c75009d72ab73e3d
xPeski <alrodr12@ucm.es>
2024-04-16 18:44:21 +0200

    EnemyData Update

--- assets/CharactersInfo/EnemyDATA.js
+++ assets/CharactersInfo/EnemyDATA.js
@@ -1,67 +1,67 @@
-export var enemigos = {
+export let enemigos = {
     Soldier: {
-        name: 'Soldier', maxHp: 50, movementRange: 2,
+        name: 'Soldier', maxHp: 90, movementRange: 4, AttackRange:1,
         spriteIndex: 0
     },
     Lancer: {
-        name: 'Lancer', maxHp: 50, movementRange: 1,
-        spriteIndex: 2
+        name: 'Lancer', maxHp: 85, movementRange: 4, AttackRange:2,
+        spriteIndex: 1
     },
     Archer:{
-        name: 'Archer', maxHp: 30, movementRange: 4,
-        spriteIndex: 3
+        name: 'Archer', maxHp: 80, movementRange: 3, AttackRange:3,
+        spriteIndex: 2
     },
     Rogue:{
-        name:'Rogue', maxHp:30, movementRange:4,
-        spriteIndex: 4
+        name:'Rogue', maxHp:70, movementRange:4, AttackRange:1,
+        spriteIndex: 3
     },
     Berserker_orc:{
-        name:'Berserker_orc',maxHp:40,movementRange:3,
-        spriteIndex: 5
+        name:'Berserker_orc',maxHp:80,movementRange:3, AttackRange:1,
+        spriteIndex: 4
     },
     Lancer_orc:{
-        name:'Lancer_orc',maxHp:30,movementRange:3,
-        spriteIndex:6
+        name:'Lancer_orc',maxHp:70,movementRange:3,AttackRange:2,
+        spriteIndex:5
     },
     Brute_orc:{
-        name:'Brute_orc', maxHp:25, movementRange:4,
-        spriteIndex:7
+        name:'Brute_orc', maxHp:60, movementRange:4,AttackRange:1,
+        spriteIndex:6
     },
     Slingshooter_orc:{
-        name:'Slingshooter_orc',maxHp:35,movementRange:3,
-        spriteIndex:8
+        name:'Slingshooter_orc',maxHp:35,movementRange:3,AttackRange:2,
+        spriteIndex:7
     },
     Shadowy_Rogue: {
-        name: 'Shadowy_Rogue', maxHp: 50, movementRange: 3,
-        spriteIndex: 9
+        name: 'Shadowy_Rogue', maxHp: 50, movementRange: 3,AttackRange:1,
+        spriteIndex: 8
     },
     Wizard: {
-        name: 'Wizard', maxHp: 50, movementRange: 3,
-        spriteIndex: 10
+        name: 'Wizard', maxHp: 40, movementRange: 3,AttackRange:3,
+        spriteIndex: 9
     },
     Imp: {
-        name: 'Imp', maxHp: 50, movementRange: 3,
-        spriteIndex: 11
+        name: 'Imp', maxHp: 25, movementRange: 3,AttackRange:2,
+        spriteIndex: 10
     },
     Crossbowman: {
-        name: 'Crossbowman', maxHp: 50, movementRange: 3,
-        spriteIndex: 12
+        name: 'Crossbowman', maxHp: 20, movementRange: 3,AttackRange:3,
+        spriteIndex: 11
     },
     Spider: {
-        name: 'Spider', maxHp: 50, movementRange: 3,
-        spriteIndex: 13
+        name: 'Spider', maxHp: 15, movementRange: 3,AttackRange:1,
+        spriteIndex: 12
     },
     Slime: {
-        name: 'Slime', maxHp: 50, movementRange: 3,
-        spriteIndex: 14
+        name: 'Slime', maxHp: 20, movementRange: 3,AttackRange:1,
+        spriteIndex: 13
     },
     Bat: {
-        name: 'Bat', maxHp: 50, movementRange: 3,
-        spriteIndex: 15
+        name: 'Bat', maxHp: 10, movementRange: 3,AttackRange:1,
+        spriteIndex: 14
     },
     Ghost: {
-        name: 'Ghost', maxHp: 50, movementRange: 3,
-        spriteIndex: 16
+        name: 'Ghost', maxHp: 15, movementRange: 3,AttackRange:1,
+        spriteIndex: 15
     }
     
 }
